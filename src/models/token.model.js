987e13ae9cf9a4944122a6c93b50bdf6fb9174ba
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-06 15:42:16 +0300

    token

---
+++ src/models/token.model.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const {mongoose} = require("../configs/dbConnection")
+
+const TokenSchema = new mongoose.Schema(
+
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Personnel",
+            index:true,
+            required:index,
+            unique:true
+        },
+        token:{
+            type:String,
+            trim:true,
+            required:true,
+            index:true,
+            unique:true
+        }
+    }, {
+        collection:"tokens", timestamps:true
+    }
+)
+
+module.exports = mongoose.model("Token", TokenSchema)
---
+++ src/routes/token.router.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const {isAdmin} = require("../middlewares/permission")
+const router = require("express").Router()
+const {list,create,update,delete:deleteToken} = require("../controllers/token.controller")
+
+router.use(isAdmin)
+
+router.route("/")
+.get(list)
+.post(create)
+
+router.route("/:id")
+.get(read)
+.post(update)
+.patch(update)
+.delete(deleteToken)
+
+module.exports = router
