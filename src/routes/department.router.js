231320c56db2bb5ddac345a665d30e7a4e251604
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-07 13:44:01 +0300

    permission in department

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -2,15 +2,16 @@
 
 const router = require("express").Router();
 const department = require("../controllers/department.controller");
+const permission = require("../middlewares/permission")
 
-router.route("/").get(department.list).post(department.create);
+router.route("/").get(department.list).post(permission.isAdmin, department.create);
 
 router
     .route("/:id")
-    .get(department.read)
-    .put(department.update)
-    .patch(department.update)
-    .delete(department.delete);
+    .get(permission.isLogin,department.read)
+    .put(permission.isAdmin,department.update)
+    .patch(permission.isAdmin,department.update)
+    .delete(permission.isAdmin,department.delete);
 
 router.get("/:id/personnel", department.personnels)
 module.exports = router;
